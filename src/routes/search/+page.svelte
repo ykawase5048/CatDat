0437910ab3a484308a39002e8877c42991d8338b
Script Raccoon <scriptraccoon@gmail.com>
2025-01-18 13:55:05 +0100

    improve search feature
    - add and remove input fields
    - datalist for easy autompletion
    - improve design
    - mobile and desktop design
    - explain usage for user

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -2,16 +2,33 @@
 	import { properties } from '$lib/dictionaries/properties'
 	import { get_suitable_categories } from './search'
 
-	let expected_properties = $state<string[]>([])
-	let expected_non_properties = $state<string[]>([])
+	let expected_properties = $state<string[]>([''])
+	let expected_non_properties = $state<string[]>([''])
 
 	let suitable_categories = $derived(
-		get_suitable_categories(expected_properties, expected_non_properties),
+		get_suitable_categories(
+			expected_properties.filter(Boolean),
+			expected_non_properties.filter(Boolean),
+		),
 	)
 
-	let no_selection = $derived(
-		expected_properties.length === 0 && expected_non_properties.length === 0,
-	)
+	function add_property() {
+		expected_properties.push('')
+	}
+
+	function remove_property() {
+		if (expected_properties.length === 0) return
+		expected_properties.pop()
+	}
+
+	function add_non_property() {
+		expected_non_properties.push('')
+	}
+
+	function remove_non_property() {
+		if (expected_non_properties.length === 0) return
+		expected_non_properties.pop()
+	}
 </script>
 
 <svelte:head>
@@ -20,49 +37,71 @@
 
 <h2>Search for categories</h2>
 
-<p>
-	On this page you can search for categories that satisfy a set of properties and do not
-	satisfy a set of properties at the same. For example, you can search for categories
-	that have finite limits and a zero object, but are not complete.
+<p class="hint">
+	On this page you can search for categories that satisfy a set of properties and do <i
+		>not</i
+	>
+	satisfy a set of properties at the same. For example, you can search for categories that
+	are finitely complete and pointed, but not complete.
 </p>
 
-<div class="two-columns">
-	<div>
-		<p>The category <strong>should have</strong> these properties:</p>
+<p>Looking for categories with these properties:</p>
 
-		{#each properties as property}
-			<label>
-				<input
-					type="checkbox"
-					value={property.name}
-					bind:group={expected_properties}
-				/>
-				{property.name}
-			</label>
+<section class="selection" aria-label="selection of properties">
+	<div class="inputs">
+		{#each { length: expected_properties.length } as _, i}
+			<input
+				type="text"
+				list="property-list"
+				bind:value={expected_properties[i]}
+				aria-label="property {i + 1}"
+				aria-invalid={expected_properties[i].length > 0 &&
+					properties.every((p) => p.name != expected_properties[i])}
+			/>
 		{/each}
 	</div>
+	<div class="controls">
+		<button class="button positive" aria-label="add property" onclick={add_property}
+			>+</button
+		>
+		<button
+			class="button positive"
+			aria-label="remove property"
+			onclick={remove_property}>-</button
+		>
+	</div>
+</section>
 
-	<div>
-		<p>The category <strong>should not have</strong> these properties:</p>
+<p>... and <i>not</i> with these properties:</p>
 
-		{#each properties as property}
-			<label>
-				<input
-					type="checkbox"
-					value={property.name}
-					bind:group={expected_non_properties}
-				/>
-				{property.name}
-			</label>
+<section class="selection" aria-label="selection of non-properties">
+	<div class="inputs">
+		{#each { length: expected_non_properties.length } as _, i}
+			<input
+				type="text"
+				list="property-list"
+				bind:value={expected_non_properties[i]}
+				aria-label="non-property {i + 1}"
+				aria-invalid={expected_non_properties[i].length > 0 &&
+					properties.every((p) => p.name != expected_non_properties[i])}
+			/>
 		{/each}
 	</div>
-</div>
+	<div class="controls">
+		<button class="button negative" onclick={add_non_property}>+</button>
+		<button class="button negative" onclick={remove_non_property}>-</button>
+	</div>
+</section>
+
+<datalist id="property-list">
+	{#each properties as property}
+		<option value={property.name}>{property.name}</option>
+	{/each}
+</datalist>
 
 <h2>Results</h2>
 
-{#if no_selection}
-	<p>Please select some properties to get results</p>
-{:else if suitable_categories.length === 0}
+{#if suitable_categories.length === 0}
 	<p>No categories found</p>
 {:else}
 	<ul>
@@ -75,13 +114,53 @@
 {/if}
 
 <style>
-	label {
-		display: block;
+	button {
+		font-weight: bold;
+		color: white;
 	}
+
+	button.positive {
+		background-color: var(--positive-color);
+	}
+
+	button.negative {
+		background-color: var(--negative-color);
+	}
+
+	.selection {
+		display: grid;
+		gap: 0.5rem;
+		margin-bottom: 1.5rem;
+	}
+
+	.inputs {
+		display: grid;
+		gap: 0.5rem;
+	}
+
+	.controls {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 2rem;
+	}
+
+	input[aria-invalid='true'] {
+		border-color: var(--negative-color);
+		outline: 1px solid var(--negative-color);
+	}
+
 	@media (min-width: 600px) {
-		.two-columns {
-			display: grid;
-			grid-template-columns: 1fr 1fr;
+		.inputs {
+			grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+		}
+
+		.controls {
+			display: inline-flex;
+			gap: 0.5rem;
+		}
+
+		button {
+			width: 3rem;
 		}
 	}
 </style>
