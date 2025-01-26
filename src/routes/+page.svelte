d2cad72d10a06b4bdc56349755b8c65714d986c4
Script Raccoon <scriptraccoon@gmail.com>
2025-01-26 22:45:28 +0100

    improve home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,20 @@
 </svelte:head>
 
 <p>
-	Discover categories based on specific properties, or explore the properties of your
-	favorite categories!
+	<i>CatDat</i> is a searchable database of
+	<a href="https://en.wikipedia.org/wiki/Category_theory" target="_blank">categories</a>
+	and their properties.
+</p>
+
+<p>
+	Discover categories based on specific properties or non-properties, and explore the
+	properties of your favorite categories.
 </p>
 
 <p>
 	This is a community-driven project. If you notice a category with missing information
 	or a combination of properties that hasn't been observed yet (see <a href="/missing"
-		>this list</a
+		>Missing data</a
 	>), you're welcome to contribute! Head over to our
 	<a href="https://github.com/ScriptRaccoon/catdat" target="_blank">GitHub repository</a
 	> and help us make this resource even better.
