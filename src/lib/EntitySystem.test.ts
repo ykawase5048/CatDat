13de68c05d06e7452db51f73fdf94af8ce34d8bd
Script Raccoon <scriptraccoon@gmail.com>
2025-01-29 14:27:57 +0100

    restructure test cases into one block

--- src/lib/EntitySystem.test.ts
+++ src/lib/EntitySystem.test.ts
@@ -1,47 +1,7 @@
 import { DeductionSystem } from './DeductionSystem'
 import { EntitySystem } from './EntitySystem'
 
-describe('add', () => {
-	type S = {
-		id: string
-		properties: string[]
-		non_properties: string[]
-	}
-	const deduction_system = new DeductionSystem<string>(
-		new Set(['a', 'b', 'c', 'd', 'e', 'f']),
-		[
-			{ assumptions: ['a'], conclusions: ['c'] },
-			{ assumptions: ['c', 'd'], conclusions: ['e'] },
-		],
-	)
-
-	deduction_system.init()
-
-	const entity_system = new EntitySystem<S, string>(deduction_system)
-
-	const entity_data: S = {
-		id: '1',
-		properties: ['a'],
-		non_properties: ['e'],
-	}
-
-	it('should add an entity to the list of entities', () => {
-		entity_system.add(entity_data)
-		expect(entity_system.entities.length).toBe(1)
-	})
-
-	it('should add the deduced properties, non-properties, and unknown properties', () => {
-		const entity = entity_system.add(entity_data)
-
-		expect(entity.properties).toEqual(['a'])
-		expect(entity.non_properties).toEqual(['e'])
-		expect(entity.all_properties).toEqual(['a', 'c'])
-		expect(entity.deduced_non_properties).toEqual(['d'])
-		expect(entity.unknown_properties).toEqual(['b', 'f'])
-	})
-})
-
-describe('search', () => {
+describe('EntitySystem', () => {
 	type S = {
 		id: string
 		properties: string[]
@@ -75,80 +35,89 @@ describe('search', () => {
 		non_properties: ['b'],
 	})
 
-	it('should return nothing when no properties and non-properties are specified', () => {
-		expect(entity_system.search([], [])).toHaveLength(0)
-	})
-
-	it('should return one object with property a', () => {
-		expect(entity_system.search(['a'], [])).toHaveLength(1)
-	})
-
-	it('should return no object with property a but not b', () => {
-		expect(entity_system.search(['a'], ['b'])).toHaveLength(0)
-	})
-
-	it('should return one object with properties b, c, but not a', () => {
-		expect(entity_system.search(['b', 'c'], ['a'])).toHaveLength(1)
-	})
-
-	it('should return one object with properties c, but not a, b', () => {
-		expect(entity_system.search(['c'], ['a', 'b'])).toHaveLength(1)
-	})
-
-	it('should return one object where property d is unknown', () => {
-		expect(entity_system.search([], [], ['d'])).toHaveLength(1)
-	})
-})
-
-describe('missing_basic_combinations', () => {
-	type S = {
-		id: string
-		properties: string[]
-		non_properties: string[]
-	}
-
-	const deduction_system = new DeductionSystem<string>(new Set(['a', 'b', 'c', 'd']), [
-		{ assumptions: ['a'], conclusions: ['b'] },
-		{ assumptions: ['b'], conclusions: ['c', 'd'] },
-	])
-
-	deduction_system.init()
-
-	const entity_system = new EntitySystem<S, string>(deduction_system)
-
-	entity_system.add({
-		id: '1',
-		properties: ['a'],
-		non_properties: [],
-	})
-
-	entity_system.add({
-		id: '2',
-		properties: ['b'],
-		non_properties: ['a'],
-	})
-
-	entity_system.add({
-		id: '3',
-		properties: ['c'],
-		non_properties: ['b'],
-	})
-
-	it('should return exactly three combinations', () => {
-		expect(entity_system.missing_basic_combinations).toHaveLength(3)
-		expect(entity_system.missing_basic_combinations).toContainEqual({
-			assumption: 'c',
-			negation: 'd',
+	describe('add', () => {
+		it('should add objects to the list of entities', () => {
+			expect(entity_system.entities).toHaveLength(3)
 		})
 
-		expect(entity_system.missing_basic_combinations).toContainEqual({
-			assumption: 'd',
-			negation: 'c',
+		it('should add all the deduced properties and non-properties (1)', () => {
+			const entity_1 = entity_system.entities.find((e) => e.id === '1')
+			expect(entity_1).toBeDefined()
+			if (entity_1) {
+				expect(entity_1).toEqual({
+					all_non_properties: [],
+					all_properties: ['a', 'b', 'c', 'd'],
+					deduced_non_properties: [],
+					deduced_properties: ['b', 'c', 'd'],
+					id: '1',
+					non_properties: [],
+					properties: ['a'],
+					unknown_properties: [],
+				})
+			}
 		})
 
-		expect(entity_system.missing_basic_combinations).toContainEqual({
-			assumption: 'd',
-			negation: 'b',
+		it('should add all the deduced properties and non-properties (3)', () => {
+			const entity_3 = entity_system.entities.find((e) => e.id === '3')
+			expect(entity_3).toBeDefined()
+			if (entity_3) {
+				expect(entity_3).toEqual({
+					all_non_properties: ['a', 'b'],
+					all_properties: ['c'],
+					deduced_non_properties: ['a'],
+					deduced_properties: [],
+					id: '3',
+					non_properties: ['b'],
+					properties: ['c'],
+					unknown_properties: ['d'],
+				})
+			}
+		})
+	})
+
+	describe('search', () => {
+		it('should return nothing when no properties and non-properties are specified', () => {
+			expect(entity_system.search([], [])).toHaveLength(0)
+		})
+
+		it('should return one object with property a', () => {
+			expect(entity_system.search(['a'], [])).toHaveLength(1)
+		})
+
+		it('should return no object with property a but not b', () => {
+			expect(entity_system.search(['a'], ['b'])).toHaveLength(0)
+		})
+
+		it('should return one object with properties b, c, but not a', () => {
+			expect(entity_system.search(['b', 'c'], ['a'])).toHaveLength(1)
+		})
+
+		it('should return one object with properties c, but not a, b', () => {
+			expect(entity_system.search(['c'], ['a', 'b'])).toHaveLength(1)
+		})
+
+		it('should return one object where property d is unknown', () => {
+			expect(entity_system.search([], [], ['d'])).toHaveLength(1)
+		})
+	})
+
+	describe('missing_basic_combinations', () => {
+		it('should return exactly three combinations', () => {
+			expect(entity_system.missing_basic_combinations).toHaveLength(3)
+			expect(entity_system.missing_basic_combinations).toContainEqual({
+				assumption: 'c',
+				negation: 'd',
+			})
+
+			expect(entity_system.missing_basic_combinations).toContainEqual({
+				assumption: 'd',
+				negation: 'c',
+			})
+
+			expect(entity_system.missing_basic_combinations).toContainEqual({
+				assumption: 'd',
+				negation: 'b',
+			})
 		})
 	})
 })
