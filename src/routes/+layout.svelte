938400c9c0c0d6797231aadeef684fe3275159de
Script Raccoon <scriptraccoon@gmail.com>
2025-01-30 16:43:20 +0100

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,6 +28,13 @@
 		integrity="sha384-FkTZUsHjYVyYpU6dse+5AzszY5617FqhnLpcMIIAlLKTbdmeVMO/7K6BrdHWM28V"
 		crossorigin="anonymous"
 	/>
+
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="CatDat" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 <div class="container">
