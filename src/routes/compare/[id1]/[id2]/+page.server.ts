e8a0d404bba04e1489f04d4f90b919f8f9413025
Script Raccoon <scriptraccoon@gmail.com>
2025-01-30 15:09:42 +0100

    render comparison table

--- src/routes/compare/[id1]/[id2]/+page.server.ts
+++ src/routes/compare/[id1]/[id2]/+page.server.ts
@@ -1,7 +1,32 @@
 import type { PageServerLoad } from './$types'
+import { error } from '@sveltejs/kit'
+
+import {
+	categories_dictionary_detailed,
+	category_system,
+} from '$lib/categories/categories.utils'
+import { is_valid_category } from '$lib/categories/categoryIDs'
 
 export const load: PageServerLoad = (event) => {
 	const id_1 = event.params.id1
 	const id_2 = event.params.id2
-	return { id_1, id_2 }
+
+	const is_valid = is_valid_category(id_1) && is_valid_category(id_2)
+
+	if (!is_valid) throw error(404, 'Invalid query')
+
+	const category_1 = categories_dictionary_detailed[id_1]
+	const category_2 = categories_dictionary_detailed[id_2]
+
+	const comparison_result = category_system.get_comparison(category_1, category_2)
+
+	if (!comparison_result) {
+		return error(404, 'Invalid query')
+	}
+
+	return {
+		category_1: { id: id_1, name: category_1.name },
+		category_2: { id: id_2, name: category_2.name },
+		comparison_result,
+	}
 }
--- src/routes/compare/[id1]/[id2]/+page.svelte
+++ src/routes/compare/[id1]/[id2]/+page.svelte
@@ -1,9 +1,83 @@
 <script lang="ts">
+	import {
+		faCheck,
+		faQuestion,
+		faXmark,
+		type IconDefinition,
+	} from '@fortawesome/free-solid-svg-icons'
+	import Fa from 'svelte-fa'
+
 	let { data } = $props()
+	let { category_1, category_2, comparison_result } = data
+
+	const icon_config: Record<string, IconDefinition> = {
+		true: faCheck,
+		false: faXmark,
+		null: faQuestion,
+	}
 </script>
 
-<h2>Compare two categories</h2>
+<svelte:head>
+	<title>Comparison of two categories</title>
+</svelte:head>
 
-<h3>{data.id_1}</h3>
+<h2>Comparison: {category_1.name} vs. {category_2.name}</h2>
 
-<h3>{data.id_2}</h3>
+<table>
+	<thead>
+		<tr>
+			<th>Property</th>
+			<th>
+				<a href="/category/{category_1.id}">
+					{category_1.id}
+				</a>
+			</th>
+			<th>
+				<a href="/category/{category_2.id}">
+					{category_2.id}
+				</a>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each comparison_result as [property, value_1, value_2]}
+			<tr>
+				<td>{property}</td>
+				<td class={JSON.stringify(value_1)}>
+					<Fa icon={icon_config[JSON.stringify(value_1)]} />
+				</td>
+				<td class={JSON.stringify(value_2)}>
+					<Fa icon={icon_config[JSON.stringify(value_2)]} />
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		border-spacing: 0;
+	}
+
+	th {
+		text-align: left;
+		padding-block: 0.5rem;
+	}
+
+	td:nth-child(2),
+	td:nth-child(3) {
+		width: 15%;
+	}
+
+	td.true {
+		color: var(--success-color);
+	}
+
+	td.false {
+		color: var(--error-color);
+	}
+
+	td.null {
+		color: var(--secondary-text-color);
+	}
+</style>
