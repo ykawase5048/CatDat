4c33c37833aef5c9ad1073eef5766de5b6c057d5
Script Raccoon <scriptraccoon@gmail.com>
2025-02-01 13:23:48 +0100

    change intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,10 @@
 <h2>A comprehensive database of categories and their properties</h2>
 
 <p>
-	Explore a growing collection of <a
-		href="https://en.wikipedia.org/wiki/Category_theory"
-		target="_blank">categories</a
-	>, each with detailed descriptions, properties, and non-properties. Our powerful
-	deduction system is based on a list of implications, allowing you to search for
+	<i>CatDat</i> provides a growing collection of
+	<a href="https://en.wikipedia.org/wiki/Category_theory" target="_blank">categories</a
+	>, each with detailed descriptions, properties, and non-properties. A list of
+	implications leverages a powerful deduction system, allowing you to search for
 	categories with specific properties or non-properties. Additionally, you can compare
 	categories to see how their properties align or differ.
 </p>
