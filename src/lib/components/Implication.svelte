acc77057ce9a34214dc331b2efb8ea905c946b02
Script Raccoon <scriptraccoon@gmail.com>
2025-02-03 19:33:18 +0100

    display tooltip in implication

--- src/lib/components/Implication.svelte
+++ src/lib/components/Implication.svelte
@@ -9,49 +9,62 @@
 	import type { Implication } from '$lib/commons/types'
 	import type { PropertyID } from '$lib/properties/propertyIDs'
 	import { get_property_url } from '$lib/properties/properties.utils'
+	import Tooltip from './Tooltip.svelte'
 
 	type Props = { implication: Implication; highlighted?: PropertyID }
 
 	let { implication, highlighted }: Props = $props()
 </script>
 
-{#each implication.assumptions as assumption, i}
-	<a href={get_property_url(assumption)} class:highlighted={assumption === highlighted}
-		>{assumption}</a
-	>
-	{#if i < implication.assumptions.length - 1}
-		<Fa icon={faPlus} class="operator" />
-		<span class="visually-hidden">and &nbsp;</span>
-	{/if}
-{/each}
+<div class="implication">
+	{#each implication.assumptions as assumption, i}
+		<a
+			href={get_property_url(assumption)}
+			class:highlighted={assumption === highlighted}>{assumption}</a
+		>
+		{#if i < implication.assumptions.length - 1}
+			<Fa icon={faPlus} class="operator" />
+			<span class="visually-hidden">and &nbsp;</span>
+		{/if}
+	{/each}
 
-<span aria-hidden="true">
-	{#if implication.equivalent}
-		<Fa icon={faArrowsLeftRight} class="operator" />
-	{:else}
-		<Fa icon={faArrowRight} class="operator" />
-	{/if}
-</span>
+	<span aria-hidden="true">
+		{#if implication.equivalent}
+			<Fa icon={faArrowsLeftRight} class="operator" />
+		{:else}
+			<Fa icon={faArrowRight} class="operator" />
+		{/if}
+	</span>
 
-<span class="visually-hidden">
-	{#if implication.equivalent}
-		is equivalent to &nbsp;
-	{:else}
-		implies &nbsp;
-	{/if}
-</span>
+	<span class="visually-hidden">
+		{#if implication.equivalent}
+			is equivalent to &nbsp;
+		{:else}
+			implies &nbsp;
+		{/if}
+	</span>
 
-{#each implication.conclusions as conclusion, i}
-	<a href={get_property_url(conclusion)} class:highlighted={conclusion === highlighted}
-		>{conclusion}</a
-	>
-	{#if i < implication.conclusions.length - 1}
-		<Fa icon={faPlus} class="operator" />
-		<span class="visually-hidden">and &nbsp;</span>
+	{#each implication.conclusions as conclusion, i}
+		<a
+			href={get_property_url(conclusion)}
+			class:highlighted={conclusion === highlighted}>{conclusion}</a
+		>
+		{#if i < implication.conclusions.length - 1}
+			<Fa icon={faPlus} class="operator" />
+			<span class="visually-hidden">and &nbsp;</span>
+		{/if}
+	{/each}
+
+	{#if implication.reason}
+		<Tooltip>{@html implication.reason}</Tooltip>
 	{/if}
-{/each}
+</div>
 
 <style>
+	.implication {
+		position: relative; /* for tooltip positioning */
+	}
+
 	a:not(.highlighted) {
 		text-decoration: none;
 	}
