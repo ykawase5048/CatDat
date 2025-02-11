538d65bbc6d111c6df904710f1851e82210668ca
Script Raccoon <scriptraccoon@gmail.com>
2025-02-11 12:21:45 +0100

    show all details in api endpoint

--- src/routes/api/v1/categories/+server.ts
+++ src/routes/api/v1/categories/+server.ts
@@ -1,11 +1,12 @@
+import { get_detailed_category } from '$lib/data-utils/details'
 import { CATEGORIES } from '$lib/database/categories.data'
 import { json, type RequestHandler } from '@sveltejs/kit'
 
-// TODO: add info about monomorphisms, related categories, etc.
 export const GET: RequestHandler = async (event) => {
 	const show_all = event.url.searchParams.has('all')
 	if (!show_all) return json(CATEGORIES)
-	const { category_system } = await import('$lib/data-utils/deductions')
-	const data = category_system.entities
-	return json(data)
+	const categories_with_details = CATEGORIES.map((category) =>
+		get_detailed_category(category.id),
+	)
+	return json(categories_with_details)
 }
