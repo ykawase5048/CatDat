8d7637dcdb805d2eb31a8c512490b64c0357c4f1
Script Raccoon <scriptraccoon@gmail.com>
2025-02-13 01:33:44 +0100

    add temporary test to ensure correctness of refactoring

--- src/lib/data-utils/deductions.test.ts
+++ src/lib/data-utils/deductions.test.ts
@@ -3,6 +3,7 @@ import { type PropertyID } from '$lib/database/properties.data'
 import { get_non_properties, get_properties, propertyIDs } from './data.helpers'
 import {
 	categories_with_deduced_properties,
+	categories_with_deduced_properties_dictionary,
 	implications_with_duals,
 	property_deduction_system,
 } from './deductions'
@@ -120,4 +121,93 @@ describe('categories with deduced properties', () => {
 			expect(counterexample).toBeDefined()
 		})
 	}
+})
+
+describe('TEMPORARY TEST DURING DEDUCTION REFACTORING', () => {
+	it('should have the expected properties for the category of abelian groups', () => {
+		const Ab = categories_with_deduced_properties_dictionary.Ab
+
+		const expected_deduced_properties = new Set([
+			'locally essentially small',
+			'locally finitely presentable',
+			'additive',
+			'equalizers',
+			'coequalizers',
+			'mono-regular',
+			'epi-regular',
+			'Malcev',
+			'balanced',
+			'finitely complete',
+			'Cauchy complete',
+			'preadditive',
+			'finite coproducts',
+			'finite products',
+			'terminal object',
+			'binary products',
+			'pullbacks',
+			'connected',
+			'locally presentable',
+			'exact filtered colimits',
+			'locally ℵ₁-presentable',
+			'zero morphisms',
+			'disjoint finite coproducts',
+			'inhabited',
+			'finitely cocomplete',
+			'initial object',
+			'binary coproducts',
+			'pushouts',
+			'pointed',
+			'filtered colimits',
+			'well-powered',
+			'well-copowered',
+			'complete',
+			'cocomplete',
+			'generator',
+			'wide pushouts',
+			'connected colimits',
+			'coproducts',
+			'countable coproducts',
+			'sequential colimits',
+			'filtered limits',
+			'wide pullbacks',
+			'connected limits',
+			'products',
+			'countable products',
+			'disjoint coproducts',
+			'sequential limits',
+			'Grothendieck abelian',
+			'cogenerator',
+		])
+
+		const expected_deduced_non_properties = new Set([
+			'small',
+			'cartesian closed',
+			'elementary topos',
+			'Grothendieck topos',
+			'strict initial object',
+			'strict terminal object',
+			'self-dual',
+			'groupoid',
+			'essentially small',
+			'thin',
+			'discrete',
+			'essentially discrete',
+			'finite',
+			'essentially finite',
+			'trivial',
+			'subobject classifier',
+			'infinitary distributive',
+			'distributive',
+			'left cancellative',
+			'right cancellative',
+		])
+
+		expect(new Set(Ab.deduced_properties.map((p) => p.id))).toEqual(
+			expected_deduced_properties,
+		)
+
+		expect(new Set(Ab.deduced_non_properties.map((p) => p.id))).toEqual(
+			expected_deduced_non_properties,
+		)
+	})
 })
