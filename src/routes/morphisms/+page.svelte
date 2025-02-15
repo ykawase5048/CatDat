9d1a02b022c536d5a9a845f125788b1c5df2a026
Script Raccoon <scriptraccoon@gmail.com>
2025-02-15 01:45:44 +0100

    accordion for morphism cards

--- src/routes/morphisms/+page.svelte
+++ src/routes/morphisms/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import type { MorphismImplication } from '$lib/database/morphisms/morphism-implications.data'
-	import type { MorphismType } from '$lib/database/morphisms/morphism-types.data'
+	import type {
+		MorphismType,
+		MorphismTypeID,
+	} from '$lib/database/morphisms/morphism-types.data'
+	import { slide } from 'svelte/transition'
 	import MorphismCard from './MorphismCard.svelte'
 	import MorphismImplicationItem from './MorphismImplicationItem.svelte'
+	import { prefersReducedMotion } from 'svelte/motion'
 
 	type Props = {
 		data: {
@@ -12,26 +17,53 @@
 	}
 
 	let { data }: Props = $props()
+
+	let expanded_morphism = $state<MorphismTypeID | null>(null)
+
+	function toggle_morphism(morphism: MorphismTypeID) {
+		if (expanded_morphism === morphism) {
+			expanded_morphism = null
+		} else {
+			expanded_morphism = morphism
+		}
+	}
 </script>
 
 <svelte:head>
 	<title>Morphisms</title>
 </svelte:head>
 
-<!-- TODO: create multiple pages for these portions of data -->
-
 <section>
 	<h2>Types of morphisms</h2>
 
 	<p class="hint">
 		The following {data.morphisms.length} types of morphisms are defined in the database.
+		Click on a type to see more information.
 	</p>
 
-	<div class="cards">
+	<ul>
 		{#each data.morphisms as morphism}
-			<MorphismCard {morphism} />
+			<li>
+				<button
+					class:selected={expanded_morphism === morphism.id}
+					onclick={() => toggle_morphism(morphism.id)}
+				>
+					{morphism.id}
+				</button>
+
+				{#if expanded_morphism === morphism.id}
+					<div
+						class="card-container"
+						transition:slide={{
+							duration: prefersReducedMotion.current ? 0 : 200,
+						}}
+					>
+						<MorphismCard {morphism} />
+					</div>
+				{/if}
+			</li>
 		{/each}
-	</div>
+	</ul>
 </section>
 
 <section>
@@ -50,13 +82,16 @@
 </section>
 
 <style>
-	.cards {
-		margin-top: 2rem;
-		display: grid;
-		gap: 2rem;
+	button:active,
+	button.selected {
+		color: var(--accent-color);
 	}
 
 	section {
 		margin-bottom: 2rem;
+	}
+
+	.card-container {
+		margin-block: 1rem;
 	}
 </style>
