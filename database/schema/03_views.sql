a826f3080f18abf2c6426bf8168383560bdb7d9e
Script Raccoon <scriptraccoon@gmail.com>
2026-03-11 08:21:43 +0100

    formatting

--- database/schema/03_views.sql
+++ database/schema/03_views.sql
@@ -5,17 +5,21 @@ SELECT
     i.id,
     i.is_equivalence,
     i.reason,
-    (SELECT json_group_array(property_id)
-     FROM (SELECT property_id
-           FROM implication_assumptions a
-           WHERE a.implication_id = i.id
-           ORDER BY lower(property_id))
+    (
+        SELECT json_group_array(property_id) FROM (
+            SELECT property_id
+            FROM implication_assumptions a
+            WHERE a.implication_id = i.id
+            ORDER BY lower(property_id)
+        )
     ) AS assumptions,
-    (SELECT json_group_array(property_id)
-     FROM (SELECT property_id
-           FROM implication_conclusions c
-           WHERE c.implication_id = i.id
-           ORDER BY lower(property_id))
+    (
+        SELECT json_group_array(property_id) FROM (
+            SELECT property_id
+            FROM implication_conclusions c
+            WHERE c.implication_id = i.id
+            ORDER BY lower(property_id)
+        )
     ) AS conclusions
 FROM implications i;
 
