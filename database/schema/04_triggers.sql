403d7df3cd0093c77b65648d5e401fd400b5fb90
Script Raccoon <scriptraccoon@gmail.com>
2026-03-12 11:55:49 +0100

    prevent contradictory properties

--- database/schema/04_triggers.sql
+++ database/schema/04_triggers.sql
@@ -6,6 +6,8 @@ DROP TRIGGER IF EXISTS trg_update_category_isomorphism;
 DROP TRIGGER IF EXISTS trg_update_category_epimorphism;
 DROP TRIGGER IF EXISTS trg_update_category_monomorphism;
 DROP TRIGGER IF EXISTS trg_implication_input_insert;
+DROP TRIGGER IF EXISTS trg_prevent_contradictory_property;
+DROP TRIGGER IF EXISTS trg_prevent_contradictory_non_property;
 
 
 CREATE TRIGGER trg_update_category
@@ -103,4 +105,32 @@ BEGIN
     INSERT INTO implication_conclusions (implication_id, property_id)
     SELECT NEW.id, value
     FROM json_each(NEW.conclusions);
+END;
+
+
+CREATE TRIGGER trg_prevent_contradictory_property
+BEFORE INSERT ON category_properties
+FOR EACH ROW
+WHEN EXISTS (
+    SELECT 1 FROM category_non_properties
+    WHERE
+        category_id = NEW.category_id
+        AND non_property_id = NEW.property_id
+)
+BEGIN
+    SELECT RAISE(ABORT, 'Cannot insert property since it is already marked as a non-property.');
+END;
+
+
+CREATE TRIGGER trg_prevent_contradictory_non_property
+BEFORE INSERT ON category_non_properties
+FOR EACH ROW
+WHEN EXISTS (
+    SELECT 1 FROM category_properties
+    WHERE
+        category_id = NEW.category_id
+        AND property_id = NEW.non_property_id
+)
+BEGIN
+    SELECT RAISE(ABORT, 'Cannot insert non-property since it is already marked as a property.');
 END;
