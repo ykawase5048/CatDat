7909b1d94dca892acc6151d86247401a31c14371
Script Raccoon <scriptraccoon@gmail.com>
2026-03-13 12:33:10 +0100

    change method

--- src/routes/api/db-dump/+server.ts
+++ src/routes/api/db-dump/+server.ts
@@ -1,7 +1,7 @@
 import { DB_AUTH_TOKEN_HTTP, DB_HTTP_URL, INTERNAL_API_KEY } from '$env/static/private'
 import { json } from '@sveltejs/kit'
 
-export const POST = async (event) => {
+export const GET = async (event) => {
 	const auth = event.request.headers.get('x-api-key')
 	if (auth !== INTERNAL_API_KEY) {
 		return new Response('Unauthorized', { status: 401 })
