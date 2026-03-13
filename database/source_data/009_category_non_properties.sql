f38fa02c7a8d65988d95b3f123163705918c8b37
Script Raccoon <scriptraccoon@gmail.com>
2026-03-13 15:56:29 +0100

    add reasons for some non-properties

--- database/source_data/009_category_non_properties.sql
+++ database/source_data/009_category_non_properties.sql
@@ -779,27 +779,27 @@ VALUES
 	(
 		'walking_pair',
 		'initial object',
-		NULL
+		'$0$ is not initial since it has two morphisms to $1$, and $1$ has not initial since it has no morphism to $0$.'
 	),
 	(
 		'walking_pair',
 		'zero morphisms',
-		NULL
+		'Assume that $a : 0 \to 1$ is the zero morphism from $0$ to $1$, and the other morphism is $b$. The identity of $0$ must be the zero morphism from $0$ to $0$. But then $b = b \circ \mathrm{id}_0 = b \circ 0_{0,0} = 0_{0,1} = a$, a contradiction.'
 	),
 	(
 		'walking_pair',
 		'binary products',
-		NULL
+		'We cannot have $0 \times 1 = 1$ since there is no morphism $1 \to 0$. So assume $0 \times 1 = 0$, say with projections $\mathrm{id}_0 : 0 \to 0$ and $a : 0 \to 1$. By applying the universal property to  $\mathrm{id}_0 : 0 \to 0$ and the other morphism $b : 0 \to 1$, it immediately follows $a=b$, which is a contradiction.'
 	),
 	(
 		'walking_pair',
 		'equalizers',
-		NULL
+		'The two morphisms $a,b : 0 \rightrightarrows 1$ have no equalizer, since it would have to be the identity of $0$, but $a \neq b$.'
 	),
 	(
 		'walking_pair',
 		'pullbacks',
-		NULL
+		'The two morphisms $a,b : 0 \rightrightarrows 1$ have no pullback, since it would to consist of identities $0 \leftarrow 0 \rightarrow 0$, but $a \neq b$.'
 	),
 	(
 		'walking_isomorphism',
