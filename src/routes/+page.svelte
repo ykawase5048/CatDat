00e0e7d37ab203e08a98b5e5ff6c1cb51537b89f
Script Raccoon <scriptraccoon@gmail.com>
2026-03-14 12:03:46 +0100

    add disclaimer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,13 @@
 		>categories</a
 	>, each with detailed descriptions, properties, and non-properties. It leverages a
 	powerful deduction system based on a list of implications, which allows for automatic
-	deduction of properties and non-properties from more basic ones. Additionally, you can
-	search for categories with specific properties or non-properties, and also compare
-	several categories to see how their properties align or differ.
+	deduction of properties and non-properties from more basic ones.
+</p>
+
+<p>
+	Additionally, you can search for categories with specific properties or
+	non-properties, and also compare several categories to see how their properties align
+	or differ.
 </p>
 
 <p>
@@ -23,4 +27,9 @@
 	>
 	to fill in <a class="accent" href="/missing">missing data</a>
 	or observe new combinations of properties.
+</p>
+
+<p>
+	Notice that this project is still in its early stages. Breaking changes are possible.
+	Also, the domain might change in the near future.
 </p>
