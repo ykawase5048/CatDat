8a8b7dd1b66c75fdeeca8c0fce8ad0c4f7f028b9
Script Raccoon <scriptraccoon@gmail.com>
2026-03-24 02:02:29 +0100

    add regular epis (trivial cases for now)

--- database/data/016_special_morphism_types.sql
+++ database/data/016_special_morphism_types.sql
@@ -3,4 +3,5 @@ VALUES
     ('isomorphisms', 0),
     ('monomorphisms', 1),
     ('epimorphisms', 2),
-    ('regular monomorphisms', 3);
+    ('regular monomorphisms', 3),
+    ('regular epimorphisms', 4);
---
+++ database/data/021_regular-epimorphisms.sql
@@ -0,0 +1,283 @@
+INSERT INTO special_morphisms (
+	category_id,
+	type,
+	description,
+	reason
+)
+VALUES
+(
+    '0',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    '1',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    '2',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Ab',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Abfg',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Alg(R)',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'B',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'BG',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'BGf',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'BN',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'BOn',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'CAlg(R)',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+), 
+(
+    'CMon',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'CRing',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'FI',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'FinAb',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'FinOrd',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'FinSet',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Fld',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),
+(
+    'FS',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Grp',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),
+(
+    'M-Set',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),
+(
+    'Mon',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'N',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'Noo',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'On',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'R-Mod',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'R-Mod_div',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'real_interval',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),
+(
+    'Ring',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'Rng',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+), 
+(
+    'Set',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'Set*',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'Setne',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Sh(X)',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Sh(X,Ab)',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Sp',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'sSet',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Top',
+    'regular epimorphisms',
+    'surjective quotient maps',
+    'Regular epimorphisms are surjective quotient maps by the explicit construction of coequalizers. Conversely, if $q : X \to Y$ is a surjective quotient map, then one checks that $q$ is the coequalizer of its kernel pair $X \times_Y X \rightrightarrows X$: This is true for the underlying sets, and continuity of the induced morphism follows since $q$ is a quotient map.' 
+),
+(
+    'Vect',
+    'regular epimorphisms',
+    'surjective homomorphisms',
+    'This holds in every finitary algebraic category.'
+),    
+(
+    'walking_composable_pair',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'walking_isomorphism',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'walking_morphism',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'walking_pair',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+),    
+(
+    'Z',
+    'regular epimorphisms',
+    'same as epimorphisms',
+    'This is because the category is epi-regular.'
+),    
+(
+    'Zdiv',
+    'regular epimorphisms',
+    'same as isomorphisms',
+    'This is because the category is left-cancellative.'
+);
