31394035537ab7e8cfb7c025833f608065a6d42f
ykawase5048 <yuto.kawase3435@gmail.com>
2026-04-11 20:25:03 +0900

    Add several accessibilities

--- database/data/005_implications/007_locally-presentable-implications.sql
+++ database/data/005_implications/007_locally-presentable-implications.sql
@@ -7,13 +7,6 @@ INSERT INTO implication_input (
 )
 VALUES
 (
-	'locally_finitely_presentable_condition',
-	'["locally finitely presentable"]',
-	'["locally presentable"]',
-	'Locally finitely presentable categories are by definition the locally $\aleph_0$-presentable categories.',
-	FALSE
-),
-(
 	'locally_finitely_presentable_raise',
 	'["locally finitely presentable"]',
 	'["locally ℵ₁-presentable"]',
@@ -30,7 +23,7 @@ VALUES
 (
 	'locally_presentable_consequence',
 	'["locally presentable"]',
-	'["locally essentially small", "well-powered", "well-copowered", "complete", "cocomplete", "generating set"]',
+	'["locally essentially small", "well-powered", "well-copowered", "complete", "generating set"]',
 	'For locally essential smallness, see the proof of Prop. 2.1.5 in <a href="https://bookstore.ams.org/conm-104" target="_blank">Makkai-Pare</a>. For the other non-trivial conclusions see <a href="https://ncatlab.org/nlab/show/Locally+Presentable+and+Accessible+Categories" target="_blank">Adamek-Rosicky</a>, Thm. 1.20, Cor. 1.28, Rem. 1.56, Thm. 1.58.',
 	FALSE
 ),
@@ -60,5 +53,89 @@ VALUES
 	'["locally strongly finitely presentable"]',
 	'["locally finitely presentable"]',
 	'See <a href="https://ncatlab.org/nlab/show/Locally+Presentable+and+Accessible+Categories" target="_blank">Adamek-Rosicky</a>, Cor. 3.7.',
+	FALSE
+),
+(
+	'locally_presentable_definition_finite',
+	'["locally finitely presentable"]',
+	'["finitely accessible", "cocomplete"]',
+	'This is trivial.',
+	TRUE
+),
+(
+	'locally_presentable_definition_countable',
+	'["locally ℵ₁-presentable"]',
+	'["ℵ₁-accessible", "cocomplete"]',
+	'This is trivial.',
+	TRUE
+),
+(
+	'locally_presentable_definition',
+	'["locally presentable"]',
+	'["accessible", "cocomplete"]',
+	'This is trivial.',
+	TRUE
+),
+(
+	'finitely_accessible_raise',
+	'["finitely accessible"]',
+	'["ℵ₁-accessible"]',
+	'This is because any regular cardinal is strictly smaller than its successor cardinal. See <a href="https://ncatlab.org/nlab/show/sharply+smaller+cardinal" target="_blank">nLab</a>.',
+	FALSE
+),
+(
+	'countably_accessible_special_case',
+	'["ℵ₁-accessible"]',
+	'["accessible"]',
+	'This is trivial.',
+	FALSE
+),
+(
+	'accessible_require_filtered_colimit',
+	'["finitely accessible"]',
+	'["filtered colimits"]',
+	'This is trivial.',
+	FALSE
+),
+(
+	'accessible_require_Cauchy_complete',
+	'["accessible"]',
+	'["Cauchy complete"]',
+	'This is because the walking idempotent is $\kappa$-filtered for any regular cardinal $\kappa$.',
+	FALSE
+),
+(
+	'small_accessible_characterization',
+	'["small", "accessible"]',
+	'["small", "Cauchy complete"]',
+	'See <a href="https://bookstore.ams.org/conm-104" target="_blank">Makkai-Pare</a>, Thm. 2.2.2.',
+	TRUE
+),
+(
+	'locally_presentable_another_definition',
+	'["locally presentable"]',
+	'["accessible", "complete"]',
+	'See <a href="https://bookstore.ams.org/conm-104" target="_blank">Makkai-Pare</a>, Thm. 6.1.4.',
+	TRUE
+),
+(
+	'locally_strongly_finitely_presentable_definition',
+	'["locally strongly finitely presentable"]',
+	'["generalized variety", "cocomplete"]',
+	'This is trivial.',
+	TRUE
+),
+(
+	'generalized_variety_require_sifted_colimit',
+	'["generalized variety"]',
+	'["sifted colimits"]',
+	'This is trivial.',
+	FALSE
+),
+(
+	'generalized_variety_implies_accessible',
+	'["generalized variety"]',
+	'["ℵ₁-accessible"]',
+	'See <a href="http://www.tac.mta.ca/tac/volumes/8/n3/8-03abs.html" target="_blank">[AR01, Remark 4.8(2)]</a>.',
 	FALSE
 );
