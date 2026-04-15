f22dc1c14ab365e6adc5a4b030473d762fb30ec3
ykawase5048 <yuto.kawase3435@gmail.com>
2026-04-15 12:27:03 +0900

    Clarify sketches are small

--- database/data/003_properties/008_locally-presentable.sql
+++ database/data/003_properties/008_locally-presentable.sql
@@ -47,7 +47,7 @@ VALUES
 	<ol>
 		<li>It is equivalent to the category of models of a many-sorted finitary algebraic theory.</li>
 		<li>It is equivalent to the category of finite-product-preserving functors to $\mathbf{Set}$ from a small category with finite products (=Lawvere theory).</li>
-		<li>It is equivalent to the category of models of a finite-product sketch.</li>
+		<li>It is equivalent to the category of models of a small finite-product sketch.</li>
 		<li>It is equivalent to the Eilenberg&ndash;Moore category of a finitary (=filtered-colimit-preserving) monad on $\mathbf{Set}^S$ for some set $S$.</li>
 		<li>It is equivalent to the Eilenberg&ndash;Moore category of a sifted-colimit-preserving monad on $\mathbf{Set}^S$ for some set $S$. (cf. [<a href="https://doi.org/10.2168/LMCS-8(3:14)2012" target="_blank">KR12</a>, Proposition 3.3])</li>
 	</ol>
@@ -102,9 +102,9 @@ VALUES
 	'A category is <i>multi-algebraic</i> if it satisfies one of the following equivalent conditions:
 	<ol>
 		<li>It is a multi-cocomplete generalized variety, that is, it has multi-colimits and sifted colimits of all small diagrams, and there is a (small) set $G$ of strongly finitely presentable objects such that every object is a small sifted colimit of objects from $G$.</li>
-		<li>It is equivalent to the category of models of a (finite product, small coproduct)-sketch, shortly <i>FPC-sketch</i>.</li>
+		<li>It is equivalent to the category of models of a small (finite product, small coproduct)-sketch, shortly small <i>FPC-sketch</i>.</li>
 		<li>It is equivalent to the category of multi-finite-product-preserving functors to $\mathbf{Set}$ from a small category with multi-finite-products (<i>multi-algebraic theory</i>>). Here, multi-finite-products means multi-limits of finite discrete diagrams.</li>
-		<li>It is equivalent to the category of models of a multi-finite-product sketch.</li>
+		<li>It is equivalent to the category of models of a small multi-finite-product sketch.</li>
 	</ol>
 	Multi-algebraic categories are like locally strongly finitely presentable category but only with multi-colimits. The relation is similar as between locally finitely multi-presentable and locally finitely presentable categories.
 	For equivalence of conditions above, see [<a href="https://doi.org/10.1016/S0022-4049(01)00015-9" target="_blank">AR01a</a>, Lem. 1] and [<a href="http://www.tac.mta.ca/tac/volumes/8/n3/8-03abs.html" target="_blank">AR01b</a>, Thm. 4.4].
@@ -116,7 +116,7 @@ VALUES
 (
 	'locally multi-presentable',
 	'is',
-	'Let $\kappa$ be a regular cardinal. A category is <i>locally $\kappa$-multi-presentable</i> if it is $\kappa$-accessible and has connected limits. It is known that a category is locally $\kappa$-multi-presentable if and only if it is equivalent to the category of models of a limit-coproduct sketch; see Thm. 4.32 and the remark below in <a href="https://ncatlab.org/nlab/show/Locally+Presentable+and+Accessible+Categories" target="_blank">Adamek-Rosicky</a>. A category is called <i>locally multi-presentable</i> if it is locally $\kappa$-multi-presentable for some $\kappa$, equivalently, it is accessible and has connected limits.',
+	'Let $\kappa$ be a regular cardinal. A category is <i>locally $\kappa$-multi-presentable</i> if it is $\kappa$-accessible and has connected limits. It is known that a category is locally $\kappa$-multi-presentable if and only if it is equivalent to the category of models of a small limit-coproduct sketch; see Thm. 4.32 and the remark below in <a href="https://ncatlab.org/nlab/show/Locally+Presentable+and+Accessible+Categories" target="_blank">Adamek-Rosicky</a>. A category is called <i>locally multi-presentable</i> if it is locally $\kappa$-multi-presentable for some $\kappa$, equivalently, it is accessible and has connected limits.',
 	'https://ncatlab.org/nlab/show/locally+multipresentable+category',
 	NULL,
 	TRUE
