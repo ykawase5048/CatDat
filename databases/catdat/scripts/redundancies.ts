3cb79456cda0a15e870fd563554dc6bb6e4579f1
Script Raccoon <scriptraccoon@gmail.com>
2026-05-04 00:53:49 +0200

    check for redundant assignments of unsatisfied properties

--- databases/catdat/scripts/redundancies.ts
+++ databases/catdat/scripts/redundancies.ts
@@ -28,9 +28,27 @@ function check_redundant_category_property_assignments() {
 			assignments[category.id].satisfied.non_deduced,
 			implications,
 		)
+
 		if (redundant_satisfied_property) {
 			console.warn(
-				`🟠 Redundant assignment for ${category.id}: "${redundant_satisfied_property}" is deducible from others.`,
+				`🟠 Redundant assignment for ${category.id}: "${redundant_satisfied_property}" is deducible from other satisfied properties.`,
+			)
+
+			redundancy_count++
+		}
+
+		const redundant_unsatisfied_property = get_redundant_unsatisfied_property(
+			new Set([
+				...assignments[category.id].satisfied.non_deduced,
+				...assignments[category.id].satisfied.deduced,
+			]),
+			assignments[category.id].unsatisfied.non_deduced,
+			implications,
+		)
+
+		if (redundant_unsatisfied_property) {
+			console.warn(
+				`🟡 Redundant assignment for ${category.id}: "${redundant_unsatisfied_property}" is deducible from other unsatisfied properties.`,
 			)
 
 			redundancy_count++
@@ -39,9 +57,9 @@ function check_redundant_category_property_assignments() {
 
 	if (redundancy_count === 0) {
 		console.info('✅ No redundant assignments found')
+	} else {
+		console.info(`Found ${redundancy_count} redundant assignments`)
 	}
-
-	// TODO: also check redundancies for unsatisfied properties
 }
 
 function get_deduced_satisfied_properties(
@@ -79,6 +97,63 @@ function get_redundant_satisfied_property(
 		const deduced_properties = get_deduced_satisfied_properties(copy, implications, {
 			stop_when_found: p,
 		})
+		if (deduced_properties.has(p)) return p
+	}
+	return null
+}
+
+function get_deduced_unsatisfied_properties(
+	satisfied_properties: Set<string>,
+	unsatisfied_properties: Set<string>,
+	implications: NormalizedCategoryImplication[],
+	options?: { stop_when_found: string },
+) {
+	const deduced_unsatisfied_properties = new Set(unsatisfied_properties)
+
+	function get_next_implication() {
+		for (const implication of implications) {
+			if (!deduced_unsatisfied_properties.has(implication.conclusion)) continue
+			for (const p of implication.assumptions) {
+				const is_valid =
+					!deduced_unsatisfied_properties.has(p) &&
+					is_subset(implication.assumptions, satisfied_properties, p)
+				if (is_valid) return { implication, property: p }
+			}
+		}
+
+		return null
+	}
+
+	while (true) {
+		const next = get_next_implication()
+		if (!next) break
+
+		const { property } = next
+
+		deduced_unsatisfied_properties.add(property)
+
+		if (options?.stop_when_found === property) {
+			return deduced_unsatisfied_properties
+		}
+	}
+
+	return deduced_unsatisfied_properties
+}
+
+function get_redundant_unsatisfied_property(
+	satisfied_properties: Set<string>,
+	unsatisfied_properties: Set<string>,
+	implications: NormalizedCategoryImplication[],
+) {
+	for (const p of unsatisfied_properties) {
+		const copy = new Set(unsatisfied_properties)
+		copy.delete(p)
+		const deduced_properties = get_deduced_unsatisfied_properties(
+			satisfied_properties,
+			copy,
+			implications,
+			{ stop_when_found: p },
+		)
 		if (deduced_properties.has(p)) return p
 	}
 	return null
