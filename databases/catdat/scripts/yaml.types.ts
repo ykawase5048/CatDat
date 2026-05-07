db3f32e52bd3f07bfa63aeb85e8d778f68eb3f55
Script Raccoon <scriptraccoon@gmail.com>
2026-05-07 17:09:15 +0200

    seed all data from yaml files
    as for error logging, unfortunately, due to deferred FKs, it is currently not possible to log the exact file where the FK violation happened

---
+++ databases/catdat/scripts/yaml.types.ts
@@ -0,0 +1,108 @@
+export type ConfigYaml = {
+	tags: string[]
+	relations: {
+		relation: string
+		negation: string
+		conditional: string
+	}[]
+	special_object_types: {
+		type: string
+		dual: string
+	}[]
+	special_morphism_types: {
+		type: string
+		dual: string
+	}[]
+}
+
+export type CategoryYaml = {
+	id: string
+	name: string
+	notation: string
+	objects: string
+	morphisms: string
+	description?: string | null
+	nlab_link?: string
+	dual_category_id?: string | null
+	tags: string[]
+	related_categories: string[]
+	satisfied_properties: PropertyEntry[]
+	unsatisfied_properties: PropertyEntry[]
+	special_objects: Record<string, ObjectEntry | undefined>
+	special_morphisms: Record<string, MorphismEntry | undefined>
+	comments?: string[]
+	category_property_comments?: {
+		property_id: string
+		comment: string
+	}[]
+}
+
+type PropertyEntry = {
+	property_id: string
+	reason: string
+	check_redundancy?: boolean
+}
+
+type ObjectEntry = {
+	description: string
+}
+
+type MorphismEntry = {
+	description: string
+	reason: string
+}
+
+export type CategoryPropertyYaml = {
+	id: string
+	relation: string
+	description: string
+	nlab_link?: string | null
+	dual_property_id?: string | null
+	invariant_under_equivalences: boolean
+	related_properties: string[]
+}
+
+export type LemmaYaml = {
+	id: string
+	title: string
+	claim: string
+	proof: string
+}
+
+export type CategoryImplicationYaml = {
+	id: string
+	assumptions: string[]
+	conclusions: string[]
+	reason: string
+	is_equivalence: boolean
+}
+
+export type FunctorImplicationYaml = {
+	id: string
+	assumptions: string[]
+	source_assumptions: string[]
+	target_assumptions: string[]
+	conclusions: string[]
+	reason: string
+	is_equivalence: boolean
+}
+
+export type FunctorPropertyYaml = {
+	id: string
+	relation: string
+	description: string
+	nlab_link?: string | null
+	dual_property_id?: string | null
+	invariant_under_equivalences: boolean
+}
+
+export type FunctorYaml = {
+	id: string
+	name: string
+	source: string
+	target: string
+	description?: string | null
+	nlab_link?: string
+	satisfied_properties: PropertyEntry[]
+	unsatisfied_properties: PropertyEntry[]
+}
