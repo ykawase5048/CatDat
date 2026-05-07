fedb9071daa2b67498f51f1ec4332bc22db7770e
Script Raccoon <scriptraccoon@gmail.com>
2026-05-07 19:25:09 +0200

    generate yaml files for all lemmas

--- databases/catdat/scripts/generate-yaml.ts
+++ databases/catdat/scripts/generate-yaml.ts
@@ -3,64 +3,37 @@ import YAML from 'yaml'
 import fs from 'node:fs'
 import path from 'node:path'
 
-// TEMPORARY SCRIPT TO GENERATE THE YAML FILES FOR ALL PROPERTIES
+// TEMPORARY SCRIPT TO GENERATE THE YAML FILES FOR ALL SQL SEED DATA
 // THE CODE IS UGLY ON PURPOSE.
 
 const db = get_client()
 
-const properties = db
-	.prepare(
-		`SELECT id, relation, description, nlab_link,
-			dual_property_id, invariant_under_equivalences
-        FROM category_properties ORDER BY lower(id)`,
-	)
+const lemmas = db
+	.prepare(`SELECT id, title, claim, proof FROM lemmas ORDER BY id`)
 	.all() as {
 	id: string
-	relation: string
-	description: string
-	nlab_link?: string | null
-	invariant_under_equivalences: number | Boolean
-	dual_property_id?: string | null
+	title: string
+	claim: string
+	proof: string
 }[]
 
-console.info(properties.length)
-
-for (const property of properties) {
-	if (!property.nlab_link) delete property.nlab_link
-	if (!property.dual_property_id) delete property.dual_property_id
-
-	property.invariant_under_equivalences = Boolean(property.invariant_under_equivalences)
-
-	const related_properties = (
-		db
-			.prepare(
-				`SELECT related_property_id AS id FROM related_category_properties WHERE property_id = ?`,
-			)
-			.all(property.id) as { id: string }[]
-	).map((o) => o.id)
-
-	const full_property = {
-		...property,
-		related_properties,
-	} as any
-
-	let file_content = YAML.stringify(full_property, {
+for (const lemma of lemmas) {
+	let file_content = YAML.stringify(lemma, {
 		indent: 2,
 		lineWidth: 2000,
 	})
 
-	file_content = file_content.replace(
-		'\nrelated_properties:',
-		'\n\nrelated_properties:',
-	)
+	file_content = file_content.replace('\ntitle:', '\n\ntitle:')
+	file_content = file_content.replace('\nclaim:', '\n\nclaim:')
+	file_content = file_content.replace('\nproof:', '\n\nproof:')
 
 	const file_path = path.join(
 		process.cwd(),
 		'databases',
 		'catdat',
 		'data_yaml',
-		'category-properties',
-		`${property.id}.yaml`,
+		'lemmas',
+		`${lemma.id}.yaml`,
 	)
 	fs.writeFileSync(file_path, file_content, 'utf8')
 }
