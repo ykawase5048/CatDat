cf8bea93cfb35749b6db14fba5cf278df8cd68cc
Script Raccoon <scriptraccoon@gmail.com>
2026-06-14 17:28:47 +0200

    reorganize schema files

--- databases/catdat/schema/001_structures.sql
+++ databases/catdat/schema/001_structures.sql
@@ -41,4 +41,19 @@ CREATE TABLE structure_comments (
 
 CREATE INDEX idx_structure_comments ON structure_comments (structure_id);
 
+CREATE TABLE tags (
+    id INTEGER PRIMARY KEY,
+    tag TEXT NOT NULL,
+    type TEXT NOT NULL,
+    UNIQUE (tag, type),
+    FOREIGN KEY (type) REFERENCES structure_types (type) ON DELETE RESTRICT
+);
 
+CREATE TABLE structure_tag_assignments (
+    structure_id TEXT NOT NULL,
+    type TEXT NOT NULL,
+    tag TEXT NOT NULL,
+    PRIMARY KEY (structure_id, type, tag),
+    FOREIGN KEY (structure_id, type) REFERENCES structures (id, type) ON DELETE CASCADE,
+    FOREIGN KEY (tag, type) REFERENCES tags (tag, type) ON DELETE CASCADE
+);
