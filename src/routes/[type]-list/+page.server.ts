6dc654d5b534e157396f61b99f3a98b2d679e244
Script Raccoon <scriptraccoon@gmail.com>
2026-07-04 20:55:36 +0200

    fix prerendering issue

--- src/routes/[type]-list/+page.server.ts
+++ src/routes/[type]-list/+page.server.ts
@@ -1,6 +1,11 @@
-import { is_structure_type } from '$lib/commons/structures'
+import { is_structure_type, STRUCTURES } from '$lib/commons/structures'
 import { fetch_structures_and_tags } from '$lib/server/fetchers/structures'
 import { error } from '@sveltejs/kit'
+import type { EntryGenerator } from './$types'
+
+export const entries: EntryGenerator = () => {
+	return STRUCTURES.map((type) => ({ type }))
+}
 
 export const load = (event) => {
 	const type = event.params.type
