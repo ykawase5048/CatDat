65c698e90b6ef55ac6e27ce383127424542fdfe4
Script Raccoon <scriptraccoon@gmail.com>
2026-07-08 09:37:14 +0200

    add end-to-end test for structure selector

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -198,6 +198,61 @@ test.describe('category navigation', () => {
 	})
 })
 
+test.describe('structure selector', () => {
+	test('categories are selected by default', async ({ page }) => {
+		await page.goto('/')
+
+		const selector = page
+			.getByRole('combobox', {
+				name: 'Structure',
+				exact: true,
+			})
+			.first()
+
+		await expect(selector).toBeVisible()
+		await expect(selector).toHaveValue('category')
+	})
+
+	test('user can switch to functors', async ({ page }) => {
+		await page.goto('/', { waitUntil: 'networkidle' }) // fix race condition
+
+		const selector = page
+			.getByRole('combobox', {
+				name: 'Structure',
+				exact: true,
+			})
+			.first()
+
+		await expect(selector).toBeVisible()
+		await selector.selectOption('functor')
+
+		await expect(selector).toHaveValue('functor')
+
+		await expect(page).toHaveURL('/functor-list')
+
+		await expect(
+			page.getByRole('heading', {
+				name: 'List of functors',
+				exact: true,
+			}),
+		).toBeVisible()
+	})
+
+	test('functors are selected on a functor route', async ({ page }) => {
+		await page.goto('/functor-properties')
+
+		const selector = page
+			.getByRole('combobox', {
+				name: 'Structure',
+				exact: true,
+			})
+			.first()
+
+		await expect(selector).toBeVisible()
+		await expect(selector).toHaveValue('functor')
+	})
+})
+
 test.describe('functor navigation', () => {
 	test('user can navigate to a functor', async ({ page }) => {
 		await page.goto('/')
